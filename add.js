dd75c55cef9eee612e9c938502929dedff2fffdc
Игорь <93474580+lgor360@users.noreply.github.com>
2024-08-25 20:11:05 +0300

    Update add.js

--- add.js
+++ add.js
@@ -1,16 +1,7 @@
 if (navigator.platform === "New Nintendo 3DS") {
-  addTREEDS()
+  location.href = "https://lgor360.github.io/3dswebsite/home.html"
 } else if (navigator.platform === "Nintendo 3DS") {
-  addTREEDS()
+  location.href = "https://lgor360.github.io/3dswebsite/home.html"
 } else {
   console.log('у тебя не консоль семейтсва 3DS :D');
-}
-function addTREEDS() {
-  var link = document.createElement( "link" );
-  link.href = '/igor360website/3ds.css';
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  link.media = "screen,print";
-
-  document.getElementsByTagName( "head" )[0].appendChild( link );
 }
