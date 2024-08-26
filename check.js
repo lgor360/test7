a1427f0b1ed08f2f0b054e07727063bb96de3033
Игорь <93474580+lgor360@users.noreply.github.com>
2024-08-26 17:43:26 +0300

    Update check.js

--- check.js
+++ check.js
@@ -1,6 +1,8 @@
 if (navigator.platform === "New Nintendo 3DS") {
+  alert("извините, но эта версия сайта не для 3ds т. к. сайт не приспособлен к таким мелким экраном так что сейчас вас перекинут на приспособленную версию сайта :D");
   location.href = "https://lgor360.github.io/3dswebsite/home.html"
 } else if (navigator.platform === "Nintendo 3DS") {
+  alert("извините, но эта версия сайта не для 3ds т. к. сайт не приспособлен к таким мелким экраном так что сейчас вас перекинут на приспособленную версию сайта :D");
   location.href = "https://lgor360.github.io/3dswebsite/home.html"
 } else {
   console.log('у тебя не консоль семейтсва 3DS :D');
